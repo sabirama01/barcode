b9f79ee99cc5f41addc1166872f5dab37e0c8d65
sabirama <markcondes21@gmail.com>
2024-01-03 10:09:39 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let generate = document.getElementById("code-to-generate");
+let barcode = document.getElementById("barcode");
+
+function generateBarcode() {
+  if (navigator.onLine) {
+    if (generate.value != "") {
+      const barcodeSource = `https://barcodeapi.org/api/code128/${generate.value}`;
+      barcode.src = barcodeSource;
+    } else {
+      barcode.src = "/assets/images/no_input.png";
+    }
+  } else {
+    document.querySelector(".main").innerHTML = "<h1>ERROR 404. NO INTERNET ACCESS</h1>";
+  }
+}
+
+window.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    generateBarcode();
+  }
+});
+
+function clearInput() {
+  generate.value = "";
+  generateBarcode();
+}
